f61a07803fb58786d5a556895d91bc9c89f2df66
yebin <yebinp1102@naver.com>
2024-05-23 02:45:51 +0900

    Feat: search station page

---
+++ src/components/Loader.tsx
@@ -0,0 +1,16 @@
+import Loading from '/images/loader.svg';
+
+const Loader = () => {
+  return (
+    <div>
+      <img 
+        src={Loading}
+        alt="loader"
+        width={44}
+        height={44}
+      />
+    </div>
+  )
+}
+
+export default Loader
