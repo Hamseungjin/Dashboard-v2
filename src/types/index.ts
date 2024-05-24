7c7b30febddaaf8af810a1573237bc110434e0c1
yebin <yebinp1102@naver.com>
2024-05-25 00:20:26 +0900

    Feat: Daily report chart page

---
+++ src/types/index.ts
@@ -0,0 +1,19 @@
+export type FailureTypesType = {
+  type: string;
+  cnt: number;
+}
+
+export type FailureTimeType = {
+  time: string;
+  cnt: number | string;
+}
+
+export type UsageTimeType = {
+  time: string;
+  cnt: number | string;
+}
+
+export type UsageAgeType = {
+  age: string;
+  cnt: number | string;
+}
